479a8342cbde63080950140ba8d1652ba51bf12b
Efrat19 <efrat890089@gmail.com>
2020-01-15 11:30:08 +0200

    add gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,23 @@
-grunt.initConfig({
-  bump: {
-    options: {
-      files: ['package.json'],
-      updateConfigs: [],
-      commit: true,
-      commitMessage: 'Release v%VERSION%',
-      commitFiles: ['package.json'],
-      createTag: true,
-      tagName: 'v%VERSION%',
-      tagMessage: 'Version %VERSION%',
-      push: true,
-      pushTo: 'upstream',
-      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-      globalReplace: false,
-      prereleaseName: false,
-      metadata: '',
-      regExp: false
-    }
-  },
-})
+module.exports = function(grunt) {
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'upstream',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: '',
+        regExp: false
+      }
+    },
+  })
+}
